6daba7241b61b3bdefeabbc1cccc275df4e74e03
Nanai10a <Nanai10a@outlook.jp>
2021-05-05 03:00:13 +0900

    ✨ implemented!

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as puppeteer from "puppeteer";
+
+const username = "";
+
+const run = async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto(`https://github.com/${username}`);
+
+  await page.evaluate(() => {
+    const elementList = document.getElementsByClassName("flex-shrink-0");
+    let i = 0;
+    while (true) {
+      const element = elementList.item(i);
+      if (!element) {
+        break;
+      }
+      element.remove();
+      i++;
+    }
+
+    document.querySelector("html").setAttribute("data-color-mode", "dark");
+  });
+
+  await (await page.$(".js-yearly-contributions")).screenshot({ path: "./output.jpeg", type: "jpeg", quality: 100 });
+
+  await browser.close();
+};
+
+run().then().catch(console.error);
